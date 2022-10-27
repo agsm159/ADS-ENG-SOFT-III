f7accb977201b6381c6046dba00a0f5b50571d16
Antônio Gabriel <61834224+agsm159@users.noreply.github.com>
2022-10-26 21:04:15 -0300

    Update interface_figuraGeometrica.ts

--- atv_04_Interfaces/interface_figuraGeometrica.ts
+++ atv_04_Interfaces/interface_figuraGeometrica.ts
@@ -5,9 +5,7 @@ interface Calculos {
 
 interface IComparavel {
     comparar(formaGeometrica: Calculos) : number
-    
 }
-
 
 class Quadrado implements Calculos,IComparavel {
     base: number;
@@ -16,9 +14,6 @@ class Quadrado implements Calculos,IComparavel {
     constructor(b: number,  h: number) {
         this.base = b
         this.altura = h
-    }
-    comparar(formaGeometrica: Calculos): number {
-        throw new Error("Method not implemented.");
     }
     
     area(): number {
@@ -32,6 +27,19 @@ class Quadrado implements Calculos,IComparavel {
 
         return valorPerimetro
     }
+
+    comparar(formaGeometrica: Calculos): number {
+        var areaBase = this.area()
+        var areaComparada = formaGeometrica.area(5,5)
+        
+        if (areaBase > areaComparada) {
+            return 1
+        } else if (areaBase == areaComparada) {
+            return 0
+        } else {
+            return -1
+        }
+    }
 }
 
 
@@ -43,9 +51,7 @@ class Retangulo implements Calculos,IComparavel {
         this.base = b
         this.altura = h
     }
-    comparar(formaGeometrica: Calculos): number {
-        throw new Error("Method not implemented.");
-    }
+
 
     area(): number {
         var valorArea = this.base * this.altura;
@@ -58,6 +64,19 @@ class Retangulo implements Calculos,IComparavel {
 
         return valorPerimetro
     }
+
+    comparar(formaGeometrica: Calculos): number {   
+        var areaBase = this.area()
+        var areaComparada = formaGeometrica.area(5,5)
+        
+        if (areaBase > areaComparada) {
+            return 1
+        } else if (areaBase == areaComparada) {
+            return 0
+        } else {
+            return -1
+        }
+    }
 }
 
 
@@ -69,20 +88,31 @@ class Triangulo implements Calculos,IComparavel {
         this.base = b
         this.altura = h
     }
-    comparar(formaGeometrica: Calculos): number {
-        throw new Error("Method not implemented.");
-    }
 
+    
     area(): number {
         var valorArea = (this.base * this.altura) / 2;
-
+        
         return valorArea;
     }
-
+    
     perimetro(): number {
         var valorPerimetro = this.base * 3
-
+        
         return valorPerimetro
+    }
+
+    comparar(formaGeometrica: Calculos): number {
+        var areaBase = this.area()
+        var areaComparada = formaGeometrica.area(5,5)
+        
+        if (areaBase > areaComparada) {
+            return 1
+        } else if (areaBase == areaComparada) {
+            return 0
+        } else {
+            return -1
+        }
     }
 }
 
@@ -98,6 +128,10 @@ class teste {
         console.log(`Perimetro do Retangulo: ${retangulo.perimetro()}`)
         console.log(`Area do Triangulo: ${triangulo.area()}`)
         console.log(`Perimetro do Triangulo: ${triangulo.perimetro()}`)
+
+        console.log(`Comparação Quadrado com Retangulo: ${quadrado.comparar(retangulo)}`)
+        console.log(`Comparação Quadrado com Triangulo: ${quadrado.comparar(triangulo)}`)
+        console.log(`Comparação Retangulo com Triangulo: ${retangulo.comparar(triangulo)}`)
     }
 }
 
